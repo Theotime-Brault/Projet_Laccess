322b2551a91dc326c1a2ea7e935e536bf8ebbc2f
bsechet <brandon.sechet@gmail.com>
2018-11-19 15:59:18 +0100

    TEST

--- L@cces/web/js/menu.js
+++ L@cces/web/js/menu.js
@@ -1,10 +1,3 @@
-$( document ).ready(function(){
-  $(".button-collapse").sidenav();
-
-
-   var instance = M.Sidenav.getInstance(".mobile-demo");
-
-   $(".button-collapse").click(function() {
-     instance.open();
-   });
+$(document).ready(function () {
+    $('.sidenav').sidenav();
 });
