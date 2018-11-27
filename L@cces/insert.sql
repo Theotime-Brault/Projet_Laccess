3bdb495a00b9cf70af664569f6a773c3f9d653f6
Flo <florian.delgado4@gmail.com>
2018-11-27 09:38:27 +0100

    sql

---
+++ L@cces/insert.sql
@@ -0,0 +1,22 @@
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("courir", "fdgdfg", "fdgdfg", "fdgdfg", 9);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("patinoire", "fdgdfg", "fdgdfg", "fdgdfg", 8);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("rager", "fdgdfg", "fdgdfg", "fdgdfg", 42);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("casser", "fdgdfg", "fdgdfg", "fdgdfg", 823);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("vendre", "fdgdfg", "fdgdfg", "fdgdfg", 12);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("poubelle", "fdgdfg", "fdgdfg", "fdgdfg", 38);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("jambon", "fdgdfg", "fdgdfg", "fdgdfg", 28);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("gens", "fdgdfg", "fdgdfg", "fdgdfg", 82);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("mal", "fdgdfg", "fdgdfg", "fdgdfg", 1);
+insert into word_fr (word, videoLink, contextSentence, videoDescription, popularity) values("humour", "fdgdfg", "fdgdfg", "fdgdfg", 10);
+
+
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("father", "fdgdfg", "fdgdfg", "fdgdfg", 2);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("word", "fdgdfg", "fdgdfg", "fdgdfg", 58);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("yes", "fdgdfg", "fdgdfg", "fdgdfg", 22);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("eat", "fdgdfg", "fdgdfg", "fdgdfg", 23);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("eating", "fdgdfg", "fdgdfg", "fdgdfg", 12);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("earth", "fdgdfg", "fdgdfg", "fdgdfg", 8);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("mouse", "fdgdfg", "fdgdfg", "fdgdfg", 8);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("duck", "fdgdfg", "fdgdfg", "fdgdfg", 2);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("send", "fdgdfg", "fdgdfg", "fdgdfg", 81);
+  insert into word_en (word, videoLink, contextSentence, videoDescription, popularity) values("shoot", "fdgdfg", "fdgdfg", "fdgdfg", 0);
