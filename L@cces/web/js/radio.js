11952581c428aa1bef0a3676eb7141de65cdd63a
Flo <florian.delgado4@gmail.com>
2018-11-27 15:56:57 +0100

    fg

--- L@cces/web/js/radio.js
+++ L@cces/web/js/radio.js
@@ -1,21 +1,22 @@
-$(document).ready(funtion(){
-  if($("#btnEn").is(':checked')){
+$(document).ready(function(){
+  $("#imgFr").click(function(){
+    $("#btnEn").removeAttr("checked");
+    $("#btnFr").attr("checked", true);
+  });
+
+  $("#imgEn").click(function(){
+    $("#btnFr").removeAttr("checked");
+    $("#btnEn").attr("checked", true);
+  });
+
+  if($("#btnEn").is(":checked")){
+    $("#imgEn").attr("class","red");
     $("#imgEn").css("border", "solid red 2px");
     $("#imgFr").css("border", "solid black 2px");
   };
 
-  if($("#btnEn").is(':checked')){
+  if($("#btnEn").is(":checked")){
     $("#imgFr").css("border", "solid red 2px");
     $("#imgEn").css("border", "solid black 2px");
   };
-
-  $("#imgFr").click(function(){
-    $("#btnFr").attr('checked');
-    $("#btnEn").removeAttr('checked');
-  });
-
-  $("#imgEn").click(function(){
-    $("#btnEn").attr('checked');
-    $("#btnFr").removeAttr('checked');
-  });
-})
+});
