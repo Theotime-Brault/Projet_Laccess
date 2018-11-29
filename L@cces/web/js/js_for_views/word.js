169c985565c5a86857ba3ae30eaa7a57478cda01
bsechet <brandon.sechet@gmail.com>
2018-11-29 09:10:40 +0100

    changement word

--- L@cces/web/js/js_for_views/word.js
+++ L@cces/web/js/js_for_views/word.js
@@ -8,9 +8,13 @@ $(document).ready(function(){
         if($("#search-on").css("visibility") == "hidden" || $(".word-box").css("visibility") == "visible"){
             $("#search-on").css("visibility", "visible");
             $(".word-box").css("visibility", "hidden");
+            $("#material-icons-search").css("visibility", "hidden");
+            $("#material-icons-clear").css("visibility", "visible");
         }
         else {
             $(".word-box").css("visibility", "visible");
+            $("#material-icons-search").css("visibility", "visible");
+            $("#material-icons-clear").css("visibility", "hidden");
         };
     });
 });
