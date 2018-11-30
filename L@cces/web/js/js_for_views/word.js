7cb278d2dfa8a0b7e019cf7c132c45063037f98a
bsechet <brandon.sechet@gmail.com>
2018-11-30 10:07:29 +0100

    changement word

--- L@cces/web/js/js_for_views/word.js
+++ L@cces/web/js/js_for_views/word.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     });
 
 
+    // function search
     $('.slide').on('click', function (){
         if($("#search-on").css("visibility") == "hidden" || $(".word-box").css("visibility") == "visible"){
             $("#search-on").css("visibility", "visible");
@@ -16,5 +17,18 @@ $(document).ready(function(){
             $("#material-icons-search").css("visibility", "visible");
             $("#material-icons-clear").css("visibility", "hidden");
         };
+    });
+/*
+    $('.container-video-over').on('click', function(){
+        $(".container-video-over").toggleClass(".onclick");
+    });*/
+
+    $('.container-video-over').on('click', function(){
+        if($(".container-video-over").css("height") != "100%") {
+            $(".container-video-over").css("height", "100%");
+        }
+        else {
+            $(".container-video-over").css("height", "25%");
+        }
     });
 });
