7cbcf58b231a20388cc80ecb32ab06c0aecc2319
bsechet <brandon.sechet@gmail.com>
2018-12-18 16:32:36 +0100

    Changement

--- L@cces/web/js/js_for_views/searchBarreFocus.js
+++ L@cces/web/js/js_for_views/searchBarreFocus.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-    if($('.searchBarre').on("focus", function(){
-        $('.row-container').css('display', 'none');
-    }));
-}
+    /*
+    $('#search-barre').on('focus', function(){
+        $('.container-focus').toggleClass('container-focus-active');
+    });
+
+
+    $('#search-barre').on('blur', function(){
+        //$('.container-focus').toggleClass('container-focus');
+        $('.container-focus').css('visibility', 'visible');
+        $('.container-focus').css('display', 'block');
+    });
+*/
+    if($('#search-barre').is(":focus")){
+        $(".container-focus").css("visibility", "hidden");
+        $(".container-focus").css("display", "none");
+    } else {
+        $(".container-focus").css("visibility", "visible");
+        $(".container-focus").css("display", "block");
+    }
+});
