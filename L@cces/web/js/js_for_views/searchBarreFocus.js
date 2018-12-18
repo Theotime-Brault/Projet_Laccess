8a420911ac058d2b4284975e00d4f0099d816d36
Ekinocte <florian.delgado4@gmail.com>
2018-12-18 16:48:18 +0100

    yes

--- L@cces/web/js/js_for_views/searchBarreFocus.js
+++ L@cces/web/js/js_for_views/searchBarreFocus.js
@@ -1,21 +1,11 @@
 $(document).ready(function () {
-    /*
+
     $('#search-barre').on('focus', function(){
         $('.container-focus').toggleClass('container-focus-active');
     });
 
 
     $('#search-barre').on('blur', function(){
-        //$('.container-focus').toggleClass('container-focus');
-        $('.container-focus').css('visibility', 'visible');
-        $('.container-focus').css('display', 'block');
+        $('.container-focus').removeClass('container-focus-active');
     });
-*/
-    if($('#search-barre').is(":focus")){
-        $(".container-focus").css("visibility", "hidden");
-        $(".container-focus").css("display", "none");
-    } else {
-        $(".container-focus").css("visibility", "visible");
-        $(".container-focus").css("display", "block");
-    }
 });
