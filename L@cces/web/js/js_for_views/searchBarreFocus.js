111a269f56a4c21ec3182c12ec50b02060f66e2d
bsechet <brandon.sechet@gmail.com>
2018-12-19 14:50:13 +0100

    Changement index barre de recherche animé

--- L@cces/web/js/js_for_views/searchBarreFocus.js
+++ L@cces/web/js/js_for_views/searchBarreFocus.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
-    $('#search-barre').on('focus', function(){
-        $('.container-focus').toggleClass('container-focus-active');
-        $('#container-search').toggleClass('container-focus-active-search');
-        $('#container-search').toggleClass('container-focus-active-container-search');
-        $('main').toggleClass('container-focus-active-main');
+    $('#search-barre').on("focus", function(){
+        $("main").removeClass("container-switch-main");
+        $('.container-focus').toggleClass("container-focus-active");
+        $('#container-search').toggleClass("container-focus-active-container-search");
+        $('#container-search').toggleClass("container-focus-active-search");
+        $("main").toggleClass("container-focus-active-main");
         $('#search-on').css({
-            height: '36px',
-            'transition-duration': '200ms'});
+            height: "36px",
+            "transition-duration": "200ms"});
     });
 
 
-    $('#search-barre').on('blur', function(){
-        $('.container-focus').removeClass('container-focus-active');
-        $('#container-search').removeClass('container-focus-active-search');
-        $('main').removeClass('container-focus-active-main');
-        $('#container-search').removeClass('container-focus-active-container-search');
-        $('#search-on').css('height', '56px');
+    $("#search-barre").on("blur", function(){
+        setTimeout(function(){
+            $(".container-focus").removeClass("container-focus-active");
+            $("main").removeClass("container-focus-active-main");
+
+        },650);
+        $("#container-search").removeClass("container-focus-active-container-search");
+        $("#container-search").removeClass("container-focus-active-search");
+        $("main").toggleClass("container-switch-main");
+        $("#search-on").css({
+            height: "56px"});
     });
 });
