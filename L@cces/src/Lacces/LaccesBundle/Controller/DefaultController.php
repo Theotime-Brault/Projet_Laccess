a548a8d67dfd7a5ad4121ad291f4a705fec20659
BRAULT THEOTIME <tbraul01@sfasp2mi.univ-poitiers.fr>
2018-12-19 11:01:14 +0100

    modified:   src/Lacces/LaccesBundle/Controller/DefaultController.php 	modified:   src/Lacces/LaccesBundle/Resources/config/routing.yml 	modified:   src/Lacces/LaccesBundle/Resources/views/Contact/contact.html.twig 	new file:   src/Lacces/LaccesBundle/Resources/views/SwiftMailer/email.html.twig 	new file:   src/Lacces/LaccesBundle/Resources/views/flash_message.html.twig 	modified:   web/css/css_for_views/signaireEn.css

--- L@cces/src/Lacces/LaccesBundle/Controller/DefaultController.php
+++ L@cces/src/Lacces/LaccesBundle/Controller/DefaultController.php
@@ -2,13 +2,23 @@
 
 namespace Lacces\LaccesBundle\Controller;
 
+
 use Lacces\LaccesBundle\Entity\wordFr;
 use Lacces\LaccesBundle\Entity\wordEn;
 use Lacces\LaccesBundle\Repository\wordFrRepository;
 use Lacces\LaccesBundle\Repository\wordEnRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 
 class DefaultController extends Controller
@@ -78,9 +88,91 @@ class DefaultController extends Controller
         ]);
     }
 
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('@Lacces/Contact/contact.html.twig');
+        $firstname = $lastname = $email = $object = $message = NULL;
+
+        $contact_error_firstnamemin = NULL;
+
+        $form = $this->createFormBuilder()
+
+            ->add('firstname', TextType::class, array('constraints' => array(new NotBlank(array(//'message' => 'contact.error.firstname'
+        )), new Length(array('min' => 3,
+                             'max' => 10,
+                )))))
+
+            ->add('lastname', TextType::class, array('constraints' => array(new NotBlank(array(
+        )), new Length(array('min' => 3,
+                             'max' => 10,
+        )))))
+
+            ->add('email', TextType::class, array('constraints' => array(
+                new Assert\Email(array('checkMX' => true)),
+                new NotBlank(),
+        )))
+
+            ->add('object', TextType::class, array('constraints' => array(new Length(array('min' => 3)))))
+
+            ->add('message', TextareaType::class, array('constraints' => array(new NotBlank(array(
+        )), new Length(array('min' => 8,
+                             'max' => 10,
+        )))))
+
+        ->add('send', SubmitType::class, array('label' => 'Envoyer'))
+        ->add('reset', ResetType::class, array('label' => 'Reset'))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid() ) {
+
+            if ($request->isMethod('POST')) {
+
+                $request = Request::createFromGlobals();
+
+                $firstname = $form["firstname"]->getData();
+                $lastname = $form["lastname"]->getData();
+                $email = $form["email"]->getData();
+                $object = $form["object"]->getData();
+                $message = $form["message"]->getData();
+
+                $message = \Swift_Message::newInstance()
+                    ->setSubject($object)
+                    ->setFrom(array($email))
+                    ->setTo('theotime98@hotmail.fr')
+                    ->setCharset('utf-8')
+                    ->setContentType('text/html')
+                    ->setBody($this->render('@Lacces/SwiftMailer/email.html.twig', array(
+                        'firstname' => $firstname,
+                        'lastname' => $lastname,
+                        'email' => $email,
+                        'object' => $object,
+                        'message' => $message)));
+
+                $this->get('mailer')->send($message);
+
+                $this->addFlash('success', 'Ok');
+                $this->addFlash('sent', 'Ok');
+
+            } else {
+                $this->addFlash('error', 'Can\'t be reached like this');
+            }
+        }
+        else if (($form->isValid() === FALSE) && ($request->isMethod('POST'))) {
+
+            $this->addFlash('error', 'Can\'t be reached like this');
+            $this->addFlash('not_sent', 'Not Ok');
+
+        }
+
+        return $this->render('@Lacces/Contact/contact.html.twig', array(
+            'form'      => $form->createView(),
+            'firstname' => $firstname,
+            'lastname'  => $lastname,
+            'email'     => $email,
+            'object'    => $object,
+            'message'   => $message
+        ));
     }
 
     public function mentionsAction()
@@ -92,4 +184,5 @@ class DefaultController extends Controller
     {
         return $this->render('@Lacces/FAQ/faq.html.twig');
     }
+
 }
