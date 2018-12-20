798b7002f090687f7b9905f35ec760cd3c0dddda
Ekinocte <florian.delgado4@gmail.com>
2018-12-20 14:53:39 +0100

    hjlhjl

--- L@cces/web/js/js_for_views/base.js
+++ L@cces/web/js/js_for_views/base.js
@@ -3,25 +3,11 @@ $(document).ready(function () {
     $('.sidenav').sidenav({
         closeOnClick: true,
         draggable: true,
-        /*isOpen: function (el) {
-            console.log("ouvert");
-        }*/
     });
 
     $('.collapsible').collapsible();
 
     $('#collapsible-header-click').on('click', function () {
         $('.material-icons').toggleClass('rotate')
-    });
-
-
-    $(document).ready(function(){
-        $('input.autocomplete').autocomplete({
-            data: {
-                "Apple": null,
-                "Microsoft": null,
-                "Google": 'https://placehold.it/250x250'
-            },
-        });
     });
 });
