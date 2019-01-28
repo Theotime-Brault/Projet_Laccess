4f3a5802a3c6ad7a1dd462f8f038ec9a6456fc9f
bsechet <brandon.sechet@gmail.com>
2019-01-28 14:03:25 +0100

    Changement majeur oui oui

--- L@cces/web/js/js_for_views/word.js
+++ L@cces/web/js/js_for_views/word.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 
     $('.fixed-action-btn').floatingActionButton();
 
+    //Appartition de la barre de recherche au clickAction
     $("#search-box-click").on("click", function(){
        $("#search-box").css("display", "block");
        $("#search-box").css("animation", "slideInDown 400ms");
@@ -16,44 +17,41 @@ $(document).ready(function(){
         }, 150);
     });
 
-    $("#display-sentences").on("click", function(){
+
+    //Affichage des phrases au clickAction
+
+    $("#display-sentences").click(function(){
         $(".card-image").slideUp("slow");
-        if($(".context-sentence").css("display") == "none") {
-            $(".context-sentence").slideDown("fast");
-        }
-        $(".card-video-1").slideDown("slow", function(){
+        $(".context-sentence").slideDown("fast");
+        $("#card-video-1").slideDown("slow", function(){
             $("#card-video-1").css("display", "block");
             $("#card-video-1").addClass("m6");
             $("#card-video-1").removeClass("m8 offset-m2");
         });
-        $(".card-video-2").slideDown("slow", function(){
+        $("#card-video-2").slideDown("slow", function(){
             $("#card-video-2").css("display", "block");
             $("#card-video-2").addClass("m6");
             $("#card-video-2").removeClass("m8 offset-m2");
         });
     });
 
-    $("#display-videos").on("click", function(){
-        $(".context-sentence").slideUp("slow");
-        if($(".card-image").css("display") == "none") {
-            $(".card-image").slideDown("slow");
-        }
-
-
-
-        $(".card-image").css("display", "block");
+    //Affichage des vidéos au clickAction
+    $("#display-videos").click(function(){
+        $(".context-sentence").slideUp("fast");
+        $(".card-image").slideDown("slow");
         $("#block-video-1").css("display", "block");
         $("#block-video-2").css("display", "block");
-        /*
-        $("#card-video-1").css("display", "block");
-        $("#card-video-2").css("display", "block");
-        $("#card-video-2").addClass("m6");
-        $("#card-video-2").removeClass("m8 offset-m2");
-        $("#card-video-1").addClass("m6");
-        $("#card-video-1").removeClass("m8 offset-m2");*/
+        $("#card-video-1").fadeIn("slow", function () {
+            $("#card-video-1").addClass("m6");
+            $("#card-video-1").removeClass("m8 offset-m2");
+        });
+        $("#card-video-2").fadeIn("slow", function () {
+            $("#card-video-2").addClass("m6");
+            $("#card-video-2").removeClass("m8 offset-m2");
+        });
     });
 
-
+    //Lancement des 2 vidéos simultanément au clickAction (non mobile)
     $('#play-videos').on('click', function(ev) {
         $("#block-video-1").css("display", "none");
         $("#block-video-2").css("display", "none");
@@ -64,72 +62,90 @@ $(document).ready(function(){
     });
 
 
+    /*Clique sur partie grise vidéo 1
+    -   disparition carte 2
+    -   apparition phrase contexte
+    */
+
     $("#block-video-1").on("click", function(){
         $("#block-video-1").css("display", "none");
-        /*
-        $("#card-video-1").css("transition-duration", "1200ms");
-        $("#card-video-1").css("width", "100%");
-        $("#card-video-1").css("height", "100%");*/
-        $("#card-video-2").fadeOut("scale", function () {
+        if($(window).width() < 600)
+        {
+            $("#card-video-2").slideUp("slow");
+        }
+        $("#card-video-2").fadeOut("fast", function () {
             $("#card-video-1").addClass("m8 offset-m2");
             $("#card-video-1").removeClass("m6");
+            $("#context-sentence-block-video-1").slideDown("slow");
         });
-
-        /*$(".card-video-2").css("transition-duration", "1200ms");*/
-        //$(".card-video-2").css("display", "none");
-       /* $(".card-video-2").css("width", "0");
-        $(".card-video-2").css("height", "0");
-        setTimeout(function () {
-            $(".card-video-2").css("display", "none");
-        }, 800);*/
     });
 
-
+    /*Clique sur partie grise vidéo 2
+    -   disparition carte 1
+    -   apparition phrase contexte
+    */
     $("#block-video-2").on("click", function() {
         $("#block-video-2").css("display", "none");
-        /*
-        $("#card-video-2").css("transition-duration", "1200ms");
-        $("#card-video-2").css("width", "100%");
-        $("#card-video-2").css("height", "100%");
-        */
-        $("#card-video-1").fadeOut("scale", function () {
+        if($(window).width() < 600)
+        {
+            $("#card-video-1").slideUp("slow");
+        }
+        $("#card-video-1").fadeOut("fast", function () {
             $("#card-video-2").addClass("m8 offset-m2");
             $("#card-video-2").removeClass("m6");
+            $("#context-sentence-block-video-2").slideDown("slow");
         });
-
-
-
-        /*
-        $("#card-video-1").css("transition-duration", "1200ms");
-        */
-        //$("#card-video-1").css("display", "none");
-        /*
-        $("#card-video-1").css("width", "0");
-        $(".card-video-1").css("height", "0");
-        setTimeout(function () {
-            $(".card-video-1").css("display", "none");
-        }, 800);*/
     });
 
-/*
+
+
+
+
     $("#context-sentence-block-video-1").on("click", function () {
-        $(".card-video-1").css("display", "none");
-        $(".card-video-2").css("display", "block");
-        $(".card-image").css("display", "block");
-        $(".card-video-2").addClass("m8 offset-m2");
-        $(".card-video-2").removeClass("m6");
-        $("#block-video-2").css("display", "none");
+        $("#card-video-2").fadeOut("slow", function () {
+            $("#card-video-1").addClass("m8 offset-m2");
+            $("#card-video-1").removeClass("m6");
+            $("#block-video-1").fadeOut("fast");
+        });
     });
 
     $("#context-sentence-block-video-2").on("click", function () {
-        $(".card-video-2").css("display", "none");
-        $(".card-video-1").css("display", "block");
-        $(".card-image").css("display", "block");
-        $(".card-video-1").addClass("m8 offset-m2");
-        $(".card-video-1").removeClass("m6");
-        $("#block-video-1").css("display", "none");
+        $("#card-video-1").fadeOut("slow", function () {
+            $("#card-video-2").addClass("m8 offset-m2");
+            $("#card-video-2").removeClass("m6");
+            $("#block-video-2").fadeOut("fast");
+        });
     });
-*/
+
+
+
+
+
+
+
+    $("#context-sentence-block-video-1").on("click", function () {
+        if($("#card-video-2").css("display") == "none") {
+            $("#card-video-1").css("display", "none");
+            $("#card-video-2").css("display", "block");
+            $(".card-image").css("display", "block");
+            $("#card-video-2").addClass("m8 offset-m2");
+            $("#card-video-2").removeClass("m6");
+            $("#block-video-2").css("display", "none");
+        }
+    });
+
+    $("#context-sentence-block-video-2").on("click", function () {
+        if($("#card-video-1").css("display") == "none"){
+            $("#card-video-2").css("display", "none");
+            $("#card-video-1").css("display", "block");
+            $(".card-image").css("display", "block");
+            $("#card-video-1").addClass("m8 offset-m2");
+            $("#card-video-1").removeClass("m6");
+            $("#block-video-1").css("display", "none");
+        }
+    });
+
+
     if($(window).width() < 600)
     {
         $(".context-sentence").css("display", "none");
