ef7c5fc7dac5ec982cf86ba6f3b7df68ea004160
bsechet <brandon.sechet@gmail.com>
2019-01-30 16:49:01 +0100

    changement barre de recherche, ajout changement langue drapeau

--- L@cces/web/js/js_for_views/searchBarreFocus.js
+++ L@cces/web/js/js_for_views/searchBarreFocus.js
@@ -7,14 +7,21 @@ $(document).ready(function () {
         $(".container-main-active").removeClass("container-main");
         $(".container-search").toggleClass("container-search-active");
         $(".container-search-active").removeClass("container-search");
+        $("#btnSearch").fadeOut();
         $('#search-on').css({
-            height: "36px",
+            height: "40px",
             "transition-duration": "200ms"});
         $('#result').css('display', 'block');
-        $(".row-container").slideToggle("400");
+        $(".row-container").slideToggle("400", function () {
+            setTimeout(function () {
+                $("#close-barre").fadeIn();
+                $("#container-img-flag").fadeIn();
+            }, 200);
+        });
+
     });
 
-    $("#search-barre").on("blur", function(){
+    $("#close-barre").click(function(){
         $(".container-main-active").toggleClass("container-main");
         $(".container-main").removeClass("container-main-active");
         $(".container-search-active").toggleClass("container-search");
@@ -28,5 +35,25 @@ $(document).ready(function () {
             height: "56px"});
         $('#result').css('display', 'none');
         $('#result').html("");
+        $("#search-barre").blur();
+        $("#close-barre").fadeOut(10);
+        $("#container-img-flag").fadeOut(10);
+        $("#btnSearch").fadeIn();
     });
+
+    $("#img2").click(function () {
+        $("#img1").css("display , block");
+        $("#img2").css("display, none");
+        $("#img1").fadeIn();
+        $("#img2").fadeOut();
+
+    });
+
+    $("#img1").click(function () {
+        $("#img2").css("display, block");
+        $("#img1").css("display, none");
+        $("#img2").fadeIn();
+        $("#img1").fadeOut();
+    });
+
 });
