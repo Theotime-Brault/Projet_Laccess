3666214084cada3034c59f04617ba88fa673447d
bsechet <brandon.sechet@gmail.com>
2019-01-31 15:55:08 +0100

    changement page word, liste déroulante

---
+++ L@cces/src/Lacces/LaccesBundle/Controller/SecurityController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brandon-PC
+ * Date: 31/01/2019
+ * Time: 13:44
+ */
+
+namespace Lacces\LaccesBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    public function adminAction(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUserName = $authenticationUtils->getLastUsername();
+
+        return $this->render('@Lacces/Security/admin.html.twig', array(
+            'last_username' => $lastUserName,
+            'error' => $error,
+        ));
+    }
+
+    public function loginCheckAction()
+    {
+        throw new \Exception("Unexpected loginCheck action");
+    }
+
+    public function logoutAction()
+    {
+        throw new \Exception("Unexpected logout action");
+    }
+}
