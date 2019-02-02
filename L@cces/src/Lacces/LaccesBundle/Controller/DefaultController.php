818c7da38c4994db1f9bb3d2fa611e4e4acc9657
Ekinocte <florian.delgado4@gmail.com>
2019-02-02 12:35:55 +0100

    modifié :         L@cces/src/Lacces/LaccesBundle/Controller/DefaultController.php 	modifié :         L@cces/src/Lacces/LaccesBundle/Resources/views/Default/index.html.twig 	modifié :         L@cces/src/Lacces/LaccesBundle/Resources/views/SearchBarre/searchBarre.html.twig

--- L@cces/src/Lacces/LaccesBundle/Controller/DefaultController.php
+++ L@cces/src/Lacces/LaccesBundle/Controller/DefaultController.php
@@ -9,6 +9,8 @@ use Lacces\LaccesBundle\Repository\wordFrRepository;
 use Lacces\LaccesBundle\Repository\wordEnRepository;
 use Lacces\LaccesBundle\Entity\Form;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -51,11 +53,12 @@ class DefaultController extends Controller
         }else if($langue == "en"){
           $objWord = $em->getRepository('LaccesBundle:wordEn')->findByWord($word);
         }else{
-          $this->redirectToRoute('');
+          return $this->redirectToRoute('lacces_homepage');
         }
 
         if(!$objWord){
-          throw $this->createNotFoundException('Cette page n éxiste pas');
+            $this->addFlash('info', "Le mot rechercher n'existe pas.");
+            return $this->redirectToRoute('lacces_homepage');
         }
 
         return $this->render('@Lacces/Words/word.html.twig', array(
@@ -67,9 +70,58 @@ class DefaultController extends Controller
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function searchBarreAction()
+    public function searchBarreAction(Request $request)
     {
-        return $this->render('@Lacces/SearchBarre/searchBarre.html.twig');
+        /*
+        $em = $this->getDoctrine()->getManager();
+        $form = array('searchForm' => 'form');
+        $tabLangues = ["fr", "en"];
+        $formSearch = $this->createFormBuilder($form)
+            ->add('search', TextType::class, array(
+                'label'=>false,
+                'name'=>'search-barre',
+                'class'=>'search',
+                'id'=>'search-barre',
+                'autocomplete'=>'off',
+                'placeholder'=>'Rechercher un mot'
+            ))
+            ->add('langue',RadioType::class, array(
+                'choices' => $tabLangues,
+                'choice_attr' => function($l, $key, $index) {
+                    return [
+                        'name' => 'langue',
+                        'label'=>false
+                    ];
+                },
+            ))
+            ->add('<i class=\"material-icons orange-text\">search</i>', SubmitType::class, array(
+                'label'=>false,
+                'id'=>'tnSearch',
+                'class'=>'btn'
+            ))
+            ->getForm();
+
+        $formSearch->handleRequest($request);
+
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $wordSearch = $formSearch['search']->getData();
+            $langue = $formSearch['langue']->getData();
+            if($wordSearch && $langue){
+                if($langue == 'fr'){
+                    $word = $em->getRepository('LaccesBundle:wordFr')->findByWord($wordSearch);
+                }else if($langue == 'en'){
+                    $word = $em->getRepository('LaccesBundle:wordEn')->findByWord($wordSearch);
+                }
+
+                if($word){
+                    $link = "/word/".$langue."/".$word->getWord();
+                    return $this->redirectToRoute($link);
+                }
+            }
+        }*/
+        return $this->render('@Lacces/SearchBarre/searchBarre.html.twig', [
+            //'form' => $formSearch->createView(),
+        ]);
     }
 
     /**
@@ -117,37 +169,41 @@ class DefaultController extends Controller
      * @return JsonResponse
      */
     public function autoCompleteAction(Request $request){
+        if ($request -> isXmlHttpRequest()) {
 
-        $em = $this->getDoctrine()->getManager();
-        $l = $request->request->get('l');
-        $word = $request->request->get('word');
-        if($l == "fr"){
-            $words = $em->getRepository('LaccesBundle:wordFr')->findByPopularity($word."%");
-        }elseif($l == "en"){
-            $words = $em->getRepository('LaccesBundle:wordEn')->findByPopularity($word."%");
-        }else{
-            $words = null;
-        }
+            $em = $this->getDoctrine()->getManager();
+            $l = $request->request->get('l');
+            $word = $request->request->get('word');
+            if ($l == "fr") {
+                $words = $em->getRepository('LaccesBundle:wordFr')->findByPopularity($word . "%");
+            } elseif ($l == "en") {
+                $words = $em->getRepository('LaccesBundle:wordEn')->findByPopularity($word . "%");
+            } else {
+                $words = null;
+            }
 
-/*
-        if($word && $l == "fr"){
-            $wordsFr = $em->getRepository('LaccesBundle:wordFr')->findByPopularity($word."%");
-            dump($wordsFr);
-            for ($i=0; $i<sizeof($wordsFr); $i++){
-                $wordsEn[$i] = $wordsFr[$i]->getWordEns();
-            }
-        }else if($word && $l == "en"){
-            $wordsEn = $em->getRepository('LaccesBundle:wordEn')->findByPopularity($word."%");
-            for ($i=0; $i<sizeof($wordsEn); $i++){
-                $wordsFr[$i] = $wordsEn[$i]->getWordFrs();
-            }
-        }else{
-            $wordsFr = null;
-            $wordsEn = null;
+            /*
+                    if($word && $l == "fr"){
+                        $wordsFr = $em->getRepository('LaccesBundle:wordFr')->findByPopularity($word."%");
+                        dump($wordsFr);
+                        for ($i=0; $i<sizeof($wordsFr); $i++){
+                            $wordsEn[$i] = $wordsFr[$i]->getWordEns();
+                        }
+                    }else if($word && $l == "en"){
+                        $wordsEn = $em->getRepository('LaccesBundle:wordEn')->findByPopularity($word."%");
+                        for ($i=0; $i<sizeof($wordsEn); $i++){
+                            $wordsFr[$i] = $wordsEn[$i]->getWordFrs();
+                        }
+                    }else{
+                        $wordsFr = null;
+                        $wordsEn = null;
+                    }
+            */
+            return new JsonResponse(array('words' => $words));
+            //return new JsonResponse(array('wordsFr'=>$wordsFr, 'wordsEn'=>$wordsEn));
+
         }
-*/
-        return new JsonResponse(array('words'=>$words));
-        //return new JsonResponse(array('wordsFr'=>$wordsFr, 'wordsEn'=>$wordsEn));
+        return $this->redirectToRoute('lacces_homepage');
     }
 
     public function exercicesAction()
