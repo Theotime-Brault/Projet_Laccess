e6f7eb07f486e05ed1d53480ddadc275ca3eb362
bsechet <brandon.sechet@gmail.com>
2019-02-05 18:35:23 +0100

    exercice

--- L@cces/web/js/js_for_views/exercice.js
+++ L@cces/web/js/js_for_views/exercice.js
@@ -8,4 +8,12 @@ $(document).ready(function () {
         $(".card-image").slideDown(300);
         $(".card-action").addClass("hide");
     });
+
+    $("#card-content-1").on("click", function () {
+        $("#card-image-1").slideToggle("fast");
+    });
+
+    $("#card-content-2").on("click", function () {
+        $("#card-image-2").slideToggle("fast");
+    });
 });
