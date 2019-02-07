1fe69aa5037a0700bb83f4dd27054aa71e1865db
Ekinocte <florian.delgado4@gmail.com>
2019-02-07 15:08:37 +0100

    nouveau fichier : L@cces/src/Lacces/LaccesBundle/Entity/Forms/FormExRefomSign.php

---
+++ L@cces/src/Lacces/LaccesBundle/Entity/Forms/FormExRefomSign.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flo
+ * Date: 07/02/19
+ * Time: 14:51
+ */
+
+namespace Lacces\LaccesBundle\Entity\Forms;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class FormExRefomSign extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('question', TextType::class, [
+                'label'=> 'Question'
+            ])
+            ->add('solution', TextType::class, [
+                'label'=> 'Solution'
+            ]);
+    }
+
+}
