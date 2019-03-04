312becad3f37e713b4e2e3bcd69e1f430cf06f7d
bsechet <brandon.sechet@gmail.com>
2019-03-04 12:02:37 +0100

    Ajax exercice

--- L@cces/src/Lacces/LaccesBundle/Controller/ExerciceController.php
+++ L@cces/src/Lacces/LaccesBundle/Controller/ExerciceController.php
@@ -3,20 +3,23 @@
 namespace Lacces\LaccesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ExerciceController extends Controller
 {
   /**
    * @return \Symfony\Component\HttpFoundation\Response
    */
-  public function exercicesAction($langue)
+
+  public function exercicesAction(/*$langue*/)
   {
 
-    if($langue == "fr" || $langue == "en") {
+   // if($langue == "fr" || $langue == "en") {
 
       $em = $this->getDoctrine()->getManager();
 
-      $motAlea = $this->motAleatoire($langue);
+      $motAlea = $this->motAleatoire("en");
       $motAleaId = $motAlea->getId();
 
       $question = $em->getRepository('LaccesBundle:Exercise\significationVideoEn')->findByWordEn($motAleaId);
@@ -26,7 +29,7 @@ class ExerciceController extends Controller
         $this->addFlash('info', "Le mot rechercher n'existe pas.");
         return $this->redirectToRoute('lacces_homepage');
       }
-
+/*
       return $this->render('@Lacces/Exercices/exercices.html.twig', array(
         'word' => $motAlea->getWord(),
         'question' => $question[0]->getQuestion(),
@@ -35,9 +38,13 @@ class ExerciceController extends Controller
     }
 
     return $this->redirectToRoute('lacces_homepage');
-
+*/
+      return $this->render('@Lacces/Exercices/exercices.html.twig', array(
+          'word' => $motAlea->getWord(),
+          'question' => $question[0]->getQuestion(),
+      ));
   }
-
+/*
   public function motAleatoire($langue) {
 
     $em = $this->getDoctrine()->getManager();
@@ -57,6 +64,8 @@ class ExerciceController extends Controller
     }
     */
 
+
+/*
     if($langue == "fr") {
       $tabMotAlea = $em->getRepository('LaccesBundle:wordFr')->findAlea();
     } else {
@@ -68,4 +77,55 @@ class ExerciceController extends Controller
 
     return $motAlea;
   }
+*/
+
+
+    public function motAleatoire($langue) {
+
+        $em = $this->getDoctrine()->getManager();
+
+        if($langue == "fr") {
+          $tabMotAlea = $em->getRepository('LaccesBundle:wordFr')->findAlea();
+        } else {
+          $tabMotAlea = $em->getRepository('LaccesBundle:wordEn')->findAlea();
+        }
+
+        shuffle($tabMotAlea);
+        $motAlea = $tabMotAlea[0];
+
+        return $motAlea;
+  }
+
+    public function exerciceA1enAction(Request $request)
+    {
+
+        //if($langue == "fr" || $langue == "en") {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $motAlea = $this->motAleatoire("en");
+            $motAleaId = $motAlea->getId();
+
+            $question = $em->getRepository('LaccesBundle:Exercise\significationVideoEn')->findByWordEn($motAleaId);
+
+            //NE DOIS NORMALEMENT JAMAIS ARRIVER
+            if(!$motAlea){
+                $this->addFlash('info', "Le mot rechercher n'existe pas.");
+                return $this->redirectToRoute('lacces_homepage');
+            }
+
+            if($request->isXmlHttpRequest()){
+                $render =  $this->renderView('@Lacces/Exercices/Types/exerciceA1.html.twig', array(
+                    'word' => $motAlea->getWord(),
+                    'question' => $question[0]->getQuestion(),
+                ));
+                return new JsonResponse($render);
+            }else{
+                return $this->redirectToRoute('lacces_homepage');
+            }
+
+       // }
+
+
+    }
 }
