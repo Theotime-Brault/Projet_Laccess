fea525ab1d3df060a53522f8f93ebec8c347e382
bsechet <brandon.sechet@gmail.com>
2019-03-04 14:10:49 +0100

    Ajax exercice

--- L@cces/web/js/js_for_views/exercice.js
+++ L@cces/web/js/js_for_views/exercice.js
@@ -12,18 +12,8 @@ $(document).ready(function () {
         }
     });
 
-
     $("#revoir-video").on("click", function () {
         $(".card-image").slideDown(300);
         $(".card-action").addClass("hide");
     });
-
-    $("#card-content-1").on("click", function () {
-        $("#card-image-1").slideToggle("fast");
-    });
-
-    $("#card-content-2").on("click", function () {
-        $("#card-image-2").slideToggle("fast");
-    });
-
 });
